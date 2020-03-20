6f6d85d408ccba1877310355fc8ea04d99949ea6
Artanicus <artanicus@nocturnal.fi>
2020-03-20 10:09:59 +0200

    Swap left ctrl & fn around

--- keyboards/atomic/keymaps/qwertylepton/keymap.c
+++ keyboards/atomic/keymaps/qwertylepton/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_TAB,  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS },
   { KC_LCTL, KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_ENT  },
   { KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_DEL,  KC_UP,   KC_RSFT },
-  { MO(_FN), KC_LCTL, KC_LALT, KC_LGUI, MO(_RS), KC_SPC,  KC_SPC,  MO(_LW), KC_RGUI, MO(_FN), KC_POW,  KC_F2,   KC_LEFT, KC_DOWN, KC_RGHT },
+  { KC_LCTL, MO(_FN), KC_LALT, KC_LGUI, MO(_RS), KC_SPC,  KC_SPC,  MO(_LW), KC_RGUI, MO(_FN), KC_POW,  KC_F2,   KC_LEFT, KC_DOWN, KC_RGHT },
  },
 
  [_LW] = { /* LOWERED */
